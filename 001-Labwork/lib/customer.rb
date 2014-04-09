a4d964737abe8dda61d61b2a3390a5687eb3acf1
stevenmiller888 <stevenmiller888@me.com>
2014-04-09 16:40:11 -0700

    Steven Miller March WDI Cohort

---
+++ 001-Labwork/lib/customer.rb
@@ -0,0 +1,48 @@
+class Customer
+
+	attr_accessor :first_name, :last_name, :date_of_birth, :available_credit
+	attr_reader :purchases
+
+	def initialize(first_name, last_name, date_of_birth, available_credit)
+		@first_name = first_name
+		@last_name = last_name
+		@date_of_birth = date_of_birth
+		@available_credit = available_credit
+		@purchases = []
+	end
+
+	def full_name
+		"#{@first_name} #{@last_name}"
+	end
+
+	def age
+		Time.now.year - @date_of_birth.year
+	end
+
+	def use_credit(amount)
+
+		if @available_credit >= amount
+			@available_credit -= amount
+			true
+		else
+			false
+		end
+
+	end
+
+	def attempt_credit_purchase(amount, description)
+
+		if use_credit(amount)
+			@purchases.push description
+			"APPROVED: $#{'%.2f' % amount} #{description}"
+		else
+			"DECLINED: $#{'%.2f' % amount} #{description} Purchase! Only $#{'%.2f' % @available_credit} credit available"
+		end
+	end
+
+	def purchase_history
+		return @purchases.join("\\n")
+	end
+
+
+end
