00399f87c38cdc24cd85330d0dff12a326ce9bb4
shane <shanemiller500@gmail.com>
2014-04-10 08:55:10 -0700

    donr it

---
+++ 001-Labwork/lib/customer.rb
@@ -0,0 +1,57 @@
+class Customer
+
+	attr_accessor :first_name, :last_name, :date_of_birth, :credit_limit 
+	attr_reader :purchases
+ 	def initialize(first_name, last_name, date_of_birth, credit_limit)
+		@first_name = first_name
+		@last_name = last_name
+		@date_of_birth = date_of_birth
+		@credit_limit = credit_limit
+		@purchases = []
+
+ 	end
+ 
+
+
+
+
+
+	def full_name
+  		"#{@first_name} #{@last_name}"
+	end
+
+	def age
+  		Time.now.year - @date_of_birth.year
+	end
+
+	def available_credit
+  		@credit_limit
+	end	
+
+	def use_credit(amount)
+  		if @credit_limit >= amount
+    		@credit_limit-= amount
+  			true
+  		else
+  			false
+  		end
+	end
+
+	def attempt_credit_purchase(amount, description)
+  		if use_credit(amount)
+			@purchases.push description
+
+    		"APPROVED: $#{'%.2f' % amount} #{description}"
+  		else
+    		"DECLINED: $#{'%.2f' % amount} #{description} Purchase! Only $#{'%.2f' % @credit_limit} credit available"
+  		end
+
+	end
+
+	def purchase_history 
+  		@purchases.join('\n')
+	end
+
+
+
+end
